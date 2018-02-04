5674947ff606bac6304333b55da379d1a18938d0
dlphi0x <zaheer0xx@gmail.com>
2018-02-05 06:30:16 +0900

    edit viewfiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'view/keyword'
+  post "keyword/search"
+
   resources :fan_comments
   resources :reviews
   resources :authors
   resources :users
   resources :books
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
